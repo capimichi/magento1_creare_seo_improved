ad98c46082dec8f3e1b73ffceabdb44aea561cab
michele <capimichi@gmail.com>
2019-05-10 12:52:30 +0200

    Modifica attributi custom

--- app/code/local/Capimichi/CreareSeoImproved/Helper/Meta.php
+++ app/code/local/Capimichi/CreareSeoImproved/Helper/Meta.php
@@ -2,149 +2,163 @@
 
 class Capimichi_CreareSeoImproved_Helper_Meta extends Creare_CreareSeoCore_Helper_Meta
 {
-    public function getDefaultTitle($pagetype)
-    {
-        $title = $this->config($pagetype . '_title');
-        return $this->shortcode($title);
-    }
+	public function getDefaultTitle($pagetype)
+	{
+		$title = $this->config($pagetype . '_title');
+		return $this->shortcode($title);
+	}
 
-    public function getDefaultMetaDescription($pagetype)
-    {
-        $metadesc = $this->config($pagetype . '_metadesc');
-        return $this->shortcode($metadesc);
-    }
+	public function getDefaultMetaDescription($pagetype)
+	{
+		$metadesc = $this->config($pagetype . '_metadesc');
+		return $this->shortcode($metadesc);
+	}
 
-    public function getPageType()
-    {
-        $registry = new Varien_Object;
+	public function getPageType()
+	{
+		$registry = new Varien_Object;
 
-        if (Mage::registry('current_product')) {
-            $registry->_code = 'product';
-            $registry->_model = Mage::registry('current_product');
+		if (Mage::registry('current_product')) {
+			$registry->_code = 'product';
+			$registry->_model = Mage::registry('current_product');
 
-            return $registry;
+			return $registry;
 
-        } elseif (Mage::registry('current_category')) {
-            $registry->_code = 'category';
-            $registry->_model = Mage::registry('current_category');
+		} elseif (Mage::registry('current_category')) {
+			$registry->_code = 'category';
+			$registry->_model = Mage::registry('current_category');
 
-            return $registry;
+			return $registry;
 
-        } elseif (Mage::app()->getFrontController()->getRequest()->getRouteName() === 'cms') {
-            $registry->_code = 'cms';
-            $registry->_model = Mage::getSingleton('cms/page');
+		} elseif (Mage::app()->getFrontController()->getRequest()->getRouteName() === 'cms') {
+			$registry->_code = 'cms';
+			$registry->_model = Mage::getSingleton('cms/page');
 
-            return $registry;
+			return $registry;
 
-        } else {
-            return false;
+		} else {
+			return false;
 
-        }
-    }
+		}
+	}
 
-    public function config($path)
-    {
-        return Mage::getStoreConfig('creareseocore/metadata/' . $path);
-    }
+	public function config($path)
+	{
+		return Mage::getStoreConfig('creareseocore/metadata/' . $path);
+	}
 
-    public function shortcode($string)
-    {
-        $pagetype = $this->getPageType();
+	public function shortcode($string)
+	{
+		$pagetype = $this->getPageType();
 
-        $string = $this->applyParent($pagetype, $string);
+		$string = $this->applyParent($pagetype, $string);
+		$string = $this->applyCustomAttriubte($pagetype, $string);
 
-        preg_match_all("/\[(.*?)\]/", $string, $matches);
+		preg_match_all("/\[(.*?)\]/", $string, $matches);
 
-        for ($i = 0; $i < count($matches[1]); $i++) {
-            $tag = $matches[1][$i];
+		for ($i = 0; $i < count($matches[1]); $i++) {
+			$tag = $matches[1][$i];
 
-            if ($tag === "store") {
-                $string = str_replace($matches[0][$i], Mage::app()->getStore()->getName(), $string);
-            } else {
+			if ($tag === "store") {
+				$string = str_replace($matches[0][$i], Mage::app()->getStore()->getName(), $string);
+			} else {
 
-                switch ($pagetype->_code) {
-                    case 'product' :
-                        $attribute = $this->productAttribute($pagetype->_model, $tag);
-                        break;
+				switch ($pagetype->_code) {
+					case 'product' :
+					$attribute = $this->productAttribute($pagetype->_model, $tag);
+					break;
 
-                    case 'category' :
-                        $attribute = $this->attribute($pagetype->_model, $tag);
-                        break;
+					case 'category' :
+					$attribute = $this->attribute($pagetype->_model, $tag);
+					break;
 
-                    case 'cms' :
-                        $attribute = $this->attribute($pagetype->_model, $tag);
-                        break;
+					case 'cms' :
+					$attribute = $this->attribute($pagetype->_model, $tag);
+					break;
 
-                }
-                $string = str_replace($matches[0][$i], $attribute, $string);
-            }
-        }
+				}
+				$string = str_replace($matches[0][$i], $attribute, $string);
+			}
+		}
 
-        return $string;
-    }
+		return $string;
+	}
 
-    public function productAttribute($product, $attribute)
-    {
-        $data = '';
-        if ($attribute == "categories" || $attribute == "first_category") {
-            $catIds = $product->getCategoryIds();
+	public function productAttribute($product, $attribute)
+	{
+		$data = '';
+		if ($attribute == "categories" || $attribute == "first_category") {
+			$catIds = $product->getCategoryIds();
 
-            if (empty($catIds)) {
-                return $data;
-            }
+			if (empty($catIds)) {
+				return $data;
+			}
 
-            $categories = Mage::getResourceModel('catalog/category_collection')
-                ->addAttributeToSelect('name')
-                ->addAttributeToFilter('entity_id', $catIds)
-                ->addIsActiveFilter();
+			$categories = Mage::getResourceModel('catalog/category_collection')
+			->addAttributeToSelect('name')
+			->addAttributeToFilter('entity_id', $catIds)
+			->addIsActiveFilter();
 
-            if ($categories->count() < 1) {
-                return $data;
-            }
+			if ($categories->count() < 1) {
+				return $data;
+			}
 
-            if ($attribute == "categories") {
-                $categoryNames = [];
+			if ($attribute == "categories") {
+				$categoryNames = [];
 
-                foreach ($categories as $category) {
-                    $categoryNames[] = $category->getName();
-                }
+				foreach ($categories as $category) {
+					$categoryNames[] = $category->getName();
+				}
 
-                $data = implode(", ", $categoryNames);
-            }
+				$data = implode(", ", $categoryNames);
+			}
 
-            if ($attribute == "first_category") {
-                $data = $categories->getFirstItem()->getName();
-            }
-        } elseif ($product->getData($attribute)) {
-            $data = $product->getResource()
-                ->getAttribute($attribute)
-                ->getFrontend()
-                ->getValue($product);
-        }
+			if ($attribute == "first_category") {
+				$data = $categories->getFirstItem()->getName();
+			}
+		} elseif ($product->getData($attribute)) {
+			$data = $product->getResource()
+			->getAttribute($attribute)
+			->getFrontend()
+			->getValue($product);
+		}
 
-        return $data;
-    }
+		return $data;
+	}
 
-    public function attribute($model, $attribute)
-    {
-        if ($model->getData($attribute)) {
-            return $model->getData($attribute);
-        }
-    }
+	public function attribute($model, $attribute)
+	{
+		if ($model->getData($attribute)) {
+			return $model->getData($attribute);
+		}
+	}
 
-    public function applyParent($pagetype, $string)
-    {
+	public function applyParent($pagetype, $string)
+	{
 
-        if ($pagetype->_code == "category") {
-            $category = $pagetype->_model;
-            $parent = Mage::getModel('catalog/category')->load($category->getParentId());
-            $parentName = "";
-            if ($parent) {
-                $parentName = $parent->getName();
-            }
-            $string = str_replace("[parent_category]", $parentName, $string);
-        }
+		if ($pagetype->_code == "category") {
+			$category = $pagetype->_model;
+			$parent = Mage::getModel('catalog/category')->load($category->getParentId());
+			$parentName = "";
+			if ($parent) {
+				$parentName = $parent->getName();
+			}
+			$string = str_replace("[parent_category]", $parentName, $string);
+		}
 
-        return $string;
-    }
+		return $string;
+	}
+
+	public function applyCustomAttriubte($pagetype, $string)
+	{
+		if ($pagetype->_code == "product") {
+			while (preg_match("/\[custom_attribute_(.*?)\]/is", $string, $code)) {
+				$code = $code[1];
+				$product = $pagetype->_model;
+				$string = str_replace("[custom_attribute_" . $code . "]", $product->getAttribteText($code), $string);
+			}
+		}
+
+		return $string;
+	}
 }
